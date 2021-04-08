95e3298220a835a7d3b1f78525d878578bd8ee2f
jakob <jakob.germano@gmail.com>
2021-04-07 18:55:02 -0600

    commit tree a23db85f89896a09b05502ecb832bf350367151e

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if(someNumber <= 400) {
+  result = 'This one is on me!';
+  } else if (someNumber > 2000 && someNumber < 2500) {
+  result = 'I will gladly take your thirty bucks.';
+  } else {result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(genericWord) {
+  let result
+
+  switch (genericWord) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+      case 'not as generous':
+        result = 'Thank you.';
+        break;
+        case 'thanks for everything':
+          result = 'Bye.'
+          break;
+  }
+    return result;
 }
